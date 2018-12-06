196131f285f05966bdd1126d6bd0c912d01b92c5
roxma <roxma@qq.com>
2018-12-06 20:17:36 +0800

    Remove on_completed. Add on_load support.
    It seems this feature is useless. I've never use this feature.

--- autoload/ncm2_tmux.vim
+++ autoload/ncm2_tmux.vim
@@ -3,16 +3,18 @@ if get(s:, 'loaded', 0)
 endif
 let s:loaded = 1
 
-let g:ncm2_tmux#proc = yarp#py3('ncm2_tmux')
+let g:ncm2_tmux#proc = yarp#py3({
+    \ 'module': 'ncm2_tmux',
+    \ 'on_load': { -> ncm2#set_ready(g:ncm2_tmux#source)}})
 
 let g:ncm2_tmux#source = extend(
             \ get(g:, 'ncm2_tmux#source', {}), {
             \ 'name': 'tmux',
             \ 'enable': 1,
+            \ 'ready': 0,
             \ 'priority': 4,
             \ 'mark': 'T',
             \ 'on_complete': 'ncm2_tmux#on_complete',
-            \ 'on_completed': 'ncm2_tmux#on_completed',
             \ 'on_warmup': 'ncm2_tmux#on_warmup',
             \ }, 'keep')
 
@@ -26,8 +28,4 @@ endfunc
 
 func! ncm2_tmux#on_complete(ctx)
     call g:ncm2_tmux#proc.try_notify('on_complete', a:ctx)
-endfunc
-
-func! ncm2_tmux#on_completed(ctx, completed)
-    call g:ncm2_tmux#proc.try_notify('on_completed', a:ctx, a:completed)
 endfunc
--- pythonx/ncm2_tmux.py
+++ pythonx/ncm2_tmux.py
@@ -62,12 +62,10 @@ class Source(Ncm2Source):
                         m = self.match_formalize(ctx, w)
                         ud = m['user_data']
                         ud['word'] = w
-                        ud['location'] = []
                         if not matcher(base, m):
                             continue
                         matches.append(m)
-                    ud['location'].append(dict(pane_id=pane_id,
-                                               start=word.start()))
+
             except Exception as ex:
                 logger.exception(
                     'exception, failed to decode output, %s', ex)
@@ -75,75 +73,7 @@ class Source(Ncm2Source):
 
         self.complete(ctx, ctx['startccol'], matches)
 
-    def on_completed(self, ctx, completed):
-        ud = completed['user_data']
-        word = ud['word']
-        location = ud['location']
-
-        if ctx['dated']:
-            logger.debug('ctx is dated')
-            return
-
-        pat = re.compile(ctx['word_pattern'])
-
-        matches = []
-        seen = {}
-
-        panes = {}
-
-        for loc in location:
-            pane_id = loc['pane_id']
-            if pane_id in panes:
-                outs = panes[pane_id]
-            else:
-                proc = Popen(args=['tmux', 'capture-pane', '-p', '-t', '{}'.format(pane_id)],
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-                outs, errs = proc.communicate(timeout=5)
-                try:
-                    outs = outs.decode('utf-8')
-                    panes[pane_id] = outs
-                except:
-                    logger.exception(
-                        'exception, failed to decode output, %s', ex)
-                    continue
-
-            start = loc['start']
-            cur_word_end = start + len(word)
-            cur_word = outs[start: cur_word_end]
-            if word != cur_word.replace('\n', ' '):
-                logger.debug('word [%s] != cur word [%s]',
-                             word,
-                             cur_word)
-                continue
-
-            searchstr = outs[cur_word_end:]
-            mat = pat.search(searchstr)
-            if mat is None:
-                if not len(searchstr):
-                    return
-                w = searchstr
-            else:
-                w = searchstr[: mat.end()]
-
-            w = w.replace('\n', ' ')
-            if w in seen:
-                m = seen[w]
-                ud = m['user_data']
-            else:
-                m = self.match_formalize(ctx, w)
-                ud = m['user_data']
-                ud['word'] = w
-                ud['location'] = []
-                matches.append(m)
-            ud['location'].append(dict(pane_id=pane_id,
-                                       start=cur_word_end))
-
-        self.complete(ctx, ctx['ccol'], matches)
-
 
 source = Source(vim)
 
 on_complete = source.on_complete
-on_completed = source.on_completed
